1124c06af785ec44f7c38f335a18c60727b3f2b8
kingsarmamirza <kingmirzasarma@gmail.com>
2025-05-25 10:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("moon").addEventListener("click", () => {
+  alert("Madhu, your beauty rivals the glow of the moon—calm, radiant, and eternal.");
+});
+document.getElementById("flower").addEventListener("click", () => {
+  alert("Like a cherry blossom, you bloom in quiet elegance, leaving hearts in awe.");
+});
